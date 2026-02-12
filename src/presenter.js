ecec388800e98582c8ef627c62d99d86fa6708a4
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-02-11 23:40:09 -0400

    funcionalidad de multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,5 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
@@ -11,5 +12,15 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const buttonId = event.submitter.id;
+
+  let resultado;
+
+  if (buttonId === "sumar-button") {
+    resultado = sumar(firstNumber, secondNumber);
+  } else if (buttonId === "multiplicar-button") {
+    resultado = multiplicar(firstNumber, secondNumber);
+  }
+
+  div.innerHTML = "<p>Resultado: " + resultado + "</p>";
 });
